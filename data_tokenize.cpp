40443ef4d902cd0dfe3dec6ac3119df14b3eeb26
tdd2454 <tdd2454@gmail.com>
2022-05-10 17:44:22 -0400

    MoleHD

--- data_tokenize.cpp
+++ data_tokenize.cpp
@@ -2,180 +2,204 @@
 
 using namespace std;
 
-void Load_Lookup_Key(hls::stream<DMA_DATA> &input_lookup_char, hls::stream<DMA_DATA> &check_DMA_in, FPGA_DATA lookup_key[NUM_TOKEN]){
-	
-	DMA_DATA in_val;
+// void ENCODING_HV(FPGA_DATA *input,
+// 				 FPGA_DATA lookup_key[NUM_TOKEN],
+// 				 HV_VEC lookup_value[CHUNK_NUM][CHUNK_SIZE],
+// 				 int result_HV[CHUNK_NUM][CHUNK_SIZE],
+// 				 int IFM_len){
 
-	KEY0:for(int i = 0; i < NUM_TOKEN; i++){
-	#pragma HLS PIPELINE
-		in_val = input_lookup_char.read();
-		lookup_key[i] = in_val.data;
+// 	EN_HV0:for(int i = 0; i < IFM_len; i++){
+// 		EN_HV1:for(int j = 0; j < NUM_TOKEN; j++){
+// 			if(input[i] == lookup_key[j]){
+// 				int position = j;
+// 				EN_HV2:for(int k = 0; k < CHUNK_NUM; k++){
+// 	//#pragma HLS PIPELINE II=1
+// 					position = position%CHUNK_NUM;
+// 					EN_HV3:for(int l = 0; l < CHUNK_SIZE; l++){
+// 	// #pragma HLS ARRAY_RESHAPE variable=lookup_value complete dim=2
+//     // #pragma HLS ARRAY_RESHAPE variable=result_HV complete dim=2
+// 						if(lookup_value[position][l] == 1)
+// 							result_HV[k][l] = result_HV[k][l] + 1;
+// 						else
+// 							result_HV[k][l] = result_HV[k][l] - 1;
+// 					}
+// 					position = position+1;
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
-		//check input
-		// if(i == NUM_TOKEN-1){
-		// 	in_val.last = 1;
-		// }
-		// else{
-		// 	in_val.last = 0;
-		// }
-		check_DMA_in.write(in_val);
-		if(in_val.last == 1){
+// void OFM_STORE( hls::stream<HV_DATA_O>& output_dma_I,
+// 				hls::stream<HV_DATA_O>& output_dma_O,
+// 				int result_HV[CHUNK_NUM][CHUNK_SIZE],
+// 				int OFM_index){
+
+
+#define MAX_IN_LEN 50
+#define LOOKUP_KEY_LEN 256
+
+void load(hls::stream<DATA> &input_dma, hls::stream<DATA> &output_dma, int max_len, ap_int<32> *data, int &size, data_t &format)
+{
+
+	data_t in_val;
+	int i = 0;
+	// KEY0:
+	for (; i < max_len; i++)
+	{
+#pragma HLS PIPELINE
+		in_val = input_dma.read();
+		data[i] = in_val.data;
+
+		// cout << "i " << i << "keep " << in_val.keep << " strb " << in_val.strb << " user " << in_val.user << " dest " << in_val.dest << " id " << in_val.id << " last " << in_val.last << endl;
+
+		output_dma.write(in_val);
+		if (in_val.last == 1)
+		{
+			// state = 1;
 			break;
 		}
 	}
-} 
-
-void Load_Lookup_Data(hls::stream<HV_DATA_I>& input_lookup_data, hls::stream<HV_DATA_I> &check_HV_in, HV_VEC lookup_value[CHUNK_NUM][CHUNK_SIZE]){
-
-	HV_DATA_I in_data;
-
-	//#pragma HLS PIPELINE
-	DATA0:for(int i = 0; i < CHUNK_NUM; i++){
-		DATA1:for(int j = 0; j < CHUNK_SIZE; j++){
-			in_data = input_lookup_data.read();
-			lookup_value[i][j] = in_data.data;
-
-			//check input
-			// if(i == CHUNK_NUM-1 && j == CHUNK_SIZE-1){
-			// 	in_data.last = 1;
-			// }
-			// else{
-			// 	in_data.last = 0;
-			// }
-			check_HV_in.write(in_data);
-			if(in_data.last == 1){
-				break;
-			}
-		}			
-	}
-} 
-
-void Load_IFM(hls::stream<DMA_DATA>& input_dma_char, FPGA_DATA input[], int IFM_len){
-
-	DMA_DATA in_val;
-	IFM1:for(int i = 0; i < IFM_len; i++){
-	#pragma HLS PIPELINE
-		in_val = input_dma_char.read();
-		input[i] = in_val.data;
-	}
+	size = i + 1;
+	format = in_val;
 }
 
-void ENCODING_HV(FPGA_DATA *input, 
-				 FPGA_DATA lookup_key[NUM_TOKEN], 
-				 HV_VEC lookup_value[CHUNK_NUM][CHUNK_SIZE],
-				 int result_HV[CHUNK_NUM][CHUNK_SIZE],
-				 int IFM_len){
-	//Check input 
+void load_2d(hls::stream<DATA> &input_dma, hls::stream<DATA> &output_dma, ap_int<32> data[CHUNK_NUM][CHUNK_SIZE])
+{
 
-	EN_HV0:for(int i = 0; i < IFM_len; i++){
-		EN_HV1:for(int j = 0; j < NUM_TOKEN; j++){
-			if(input[i] == lookup_key[j]){
-				int position = j;
-				EN_HV2:for(int k = 0; k < CHUNK_NUM; k++){
-	//#pragma HLS PIPELINE II=1
-					position = position%CHUNK_NUM; 
-					EN_HV3:for(int l = 0; l < CHUNK_SIZE; l++){
-	#pragma HLS ARRAY_RESHAPE variable=lookup_value complete dim=2
-    #pragma HLS ARRAY_RESHAPE variable=result_HV complete dim=2
-						if(lookup_value[position][l] == 1)
-							result_HV[k][l] = result_HV[k][l] + 1;
-						else
-							result_HV[k][l] = result_HV[k][l] - 1;
-					}
-					position = position+1;
-				}
-			}
-		}
-	}
+	data_t in_val;
 
-	// for(int i=0; i<CHUNK_NUM; i++){
-	// 	for(int j=0; j<CHUNK_SIZE; j++){
-	// 		cout << result_HV[i][j] << ", \t";
-	// 	}
-	// 	cout << endl;
-	// }
-}
+	// KEY0:
+	for (int i = 0; i < CHUNK_NUM; i++)
+	{
+		for (int j = 0; j < CHUNK_SIZE; j++)
+		{
+#pragma HLS PIPELINE
+			in_val = input_dma.read();
+			data[i][j] = in_val.data;
 
-
-void OFM_STORE(hls::stream<HV_DATA_O>& output_dma_O, int result_HV[CHUNK_NUM][CHUNK_SIZE]){
-	HV_DATA_O OutStream;
-
-	OFM0:for(int i = 0; i < CHUNK_NUM; i++){
-		OFM1:for(int j = 0; j < CHUNK_SIZE; j++){
-			OutStream.data = result_HV[i][j];
-			// if(i == CHUNK_NUM-1 && j == CHUNK_SIZE-1){
-			// 	OutStream.last = 1;
-			// }
-			// else{
-			// 	OutStream.last = 0;
-			// }
-			output_dma_O.write(OutStream);
-			if(i == CHUNK_NUM-1 && j == CHUNK_SIZE-1){
+			// cout << "i " << i << "keep " << in_val.keep << " strb " << in_val.strb << " user " << in_val.user << " dest " << in_val.dest << " id " << in_val.id << " last " << in_val.last << endl;
+			cout << "2d load " << in_val.data << endl;
+			output_dma.write(in_val);
+			if (in_val.last == 1)
+			{
 				break;
 			}
 		}
 	}
-
-	cout << endl;
 }
 
+// void encode(ap_int<32> *input, ap_int<32> lookup_key[NUM_TOKEN], ap_int<32> lookup_value[CHUNK_NUM][CHUNK_SIZE],
+// 		ap_int<32> result_HV[CHUNK_NUM][CHUNK_SIZE], int size){
 
-void CREATE_HV(hls::stream<DMA_DATA> &input_dma_char, 
-					hls::stream<DMA_DATA> &input_lookup_char, 
-					hls::stream<HV_DATA_I> &input_lookup_data, 
-					hls::stream<HV_DATA_O> &output_dma_O,
-					hls::stream<DMA_DATA> &check_DMA_in,
-					hls::stream<HV_DATA_I> &check_HV_in,
-					int state,
-					int IFM_len){
+// 	EN_HV0:for(int i = 0; i < size; i++){
+// 		EN_HV1:for(int j = 0; j < NUM_TOKEN; j++){
+// 			if(input[i] == lookup_key[j]){
+// 				int position = j;
+// 				EN_HV2:for(int k = 0; k < CHUNK_NUM; k++){
+// 	//#pragma HLS PIPELINE II=1
+// 					position = position%CHUNK_NUM;
+// 					EN_HV3:for(int l = 0; l < CHUNK_SIZE; l++){
+// 	// #pragma HLS ARRAY_RESHAPE variable=lookup_value complete dim=2
+//     // #pragma HLS ARRAY_RESHAPE variable=result_HV complete dim=2
+// 						if(lookup_value[position][l] == 1)
+// 							result_HV[k][l] = result_HV[k][l] + 1;
+// 						else
+// 							result_HV[k][l] = result_HV[k][l] - 1;
+// 					}
+// 					position = position+1;
+// 				}
+// 			}
+// 		}
+// 	}
+// }
 
-	#pragma HLS INTERFACE s_axilite port=return bundle=CRTL_BUS
-	#pragma HLS INTERFACE s_axilite port=state bundle=CRTL_BUS
-	#pragma HLS INTERFACE s_axilite port=IFM_len bundle=CRTL_BUS
+void return_val (hls::stream<DATA> & output_dma, ap_int<32> data[CHUNK_NUM][CHUNK_SIZE], data_t &format)
+{
 
-	#pragma HLS INTERFACE axis port=input_dma_char
-	#pragma HLS INTERFACE axis port=input_lookup_data
-	#pragma HLS INTERFACE axis port=input_lookup_char 
-	#pragma HLS INTERFACE axis port=output_dma_O 
+	data_t out_val;
 
-	/////////Check input value/////
-	#pragma HLS INTERFACE axis port=check_DMA_in
-	#pragma HLS INTERFACE axis port=check_HV_in 
-	///////////////////////////////
+	for (int i = 0; i < 10; i++)
+	{
+		for (int j = 0; j < 10; j++)
+		{
+			out_val.data = data[i][j];
+			out_val.keep = format.keep;
+			out_val.strb = format.strb;
+			out_val.user = format.user;
+			out_val.id = format.id;
+			out_val.dest = format.dest;
 
-	static FPGA_DATA input[in_len];
-	#pragma HLS RESOURCE variable=input core=RAM_1P_BRAM
-	#pragma HLS ARRAY_PARTITION variable=input complete dim=1
+			if ((i == 10 - 1) && (j == 10 - 1))
+			{
+				out_val.last = 1;
+			}
+			else
+			{
+				out_val.last = 0;
+			}
+
+			output_dma.write(out_val);
+			cout << " i = " << i << "j = "<< j << "last " << out_val.last << endl;
+		}
+	}
+}
+
+void hv_core(hls::stream<DATA> &input_dma,
+			 hls::stream<DATA> &output_dma,
+			 // DEBUG ONLY
+			 int &state,
+			 int &cur_size)
+{
+
+#pragma HLS INTERFACE s_axilite port = return bundle = CRTL_BUS
+#pragma HLS INTERFACE s_axilite port = state bundle = CRTL_BUS
+#pragma HLS INTERFACE s_axilite port = cur_size bundle = CRTL_BUS
+
+#pragma HLS INTERFACE axis port = input_dma
+#pragma HLS INTERFACE axis port = output_dma
+
+	static ap_int<32> input[MAX_IN_LEN];
+#pragma HLS RESOURCE variable = input core = RAM_1P_BRAM
+#pragma HLS ARRAY_PARTITION variable = input complete dim = 1
 
 	static FPGA_DATA lookup_key[NUM_TOKEN];
-	#pragma HLS RESOURCE variable=lookup_key core=RAM_1P_BRAM
-	#pragma HLS ARRAY_PARTITION variable=lookup_key complete dim=1
+	// static ap_int<32> lookup_key[LOOKUP_KEY_LEN];
+#pragma HLS RESOURCE variable = lookup_key core = RAM_1P_BRAM
+#pragma HLS ARRAY_PARTITION variable = lookup_key complete dim = 1
 
-	static HV_VEC lookup_value[CHUNK_NUM][CHUNK_SIZE];
-	#pragma HLS RESOURCE variable=lookup_value core=RAM_1P_BRAM
-	#pragma HLS ARRAY_PARTITION variable=lookup_value complete dim=1
+	static ap_int<32> lookup_value[CHUNK_NUM][CHUNK_SIZE];
+#pragma HLS RESOURCE variable = lookup_value core = RAM_1P_BRAM
+#pragma HLS ARRAY_PARTITION variable = lookup_value complete dim = 1
 
-	static int result_HV[CHUNK_NUM][CHUNK_SIZE];
-	#pragma HLS RESOURCE variable=result_HV core=RAM_1P_BRAM
-	#pragma HLS ARRAY_PARTITION variable=result_HV complete dim=1
-	#pragma HLS ARRAY_PARTITION variable=result_HV complete dim=2
+	static ap_int<32> result_HV[CHUNK_NUM][CHUNK_SIZE];
 
-	if(state == 1){
-		Load_Lookup_Key(input_lookup_char, check_DMA_in, lookup_key);
-		Load_Lookup_Data(input_lookup_data, check_HV_in, lookup_value);
-		for(int i = 0; i < CHUNK_NUM; i++){
-			for(int j = 0; j < CHUNK_SIZE; j++){
-			result_HV[i][j] = 1; 
-			}
+	for(int i = 0; i < 10; i++){
+		for(int j = 0; j < 10; j++){
+			result_HV[i][j] = 10*i + j + 3;
 		}
 	}
+	state = 0;
 
-	else if(state == 2){
-		Load_IFM(input_dma_char, input, IFM_len);
-		ENCODING_HV(input, lookup_key, lookup_value, result_HV, IFM_len);
-	}
-	else if(state == 3){
-		OFM_STORE(output_dma_O, result_HV);
-	}	
+	data_t format;
 
+	load(input_dma, output_dma, NUM_TOKEN, lookup_key, cur_size, format);
+	cout << "Receive size: " << cur_size << endl;
+	state = 1;
+
+	// load_2d(input_dma, output_dma, lookup_value);
+	// state = 2;
+
+	load(input_dma, output_dma, MAX_IN_LEN, input, cur_size, format);
+	cout << "Receive size: " << cur_size << endl;
+	state = 3;
+
+	// encode(input, lookup_key, lookup_value, result_HV, size);	
+	// state = 5;
+	load(input_dma, output_dma, MAX_IN_LEN, input, cur_size, format);
+	cout << "Receive size: " << cur_size << endl;
+	state = 4;
+
+	return_val(output_dma, result_HV, format);
+	state = 5;
 }
